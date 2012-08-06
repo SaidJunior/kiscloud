e995f4c731c652a62bddc73fb268d3f1e8c2af04
sylvain.pellissier@gmail.com <sylvain.pellissier@gmail.com@4320ce56-c730-6eaf-f33c-faf3ef3ae2b0>
2012-08-06 12:10:24 +0000

    Implementation methode RPC pour récupérer profil utilisateurs ( login , mdp etc ... )

--- trunk/ kiscloud/KisCloud/src/rie6/client/admin/model/User.java
+++ trunk/ kiscloud/KisCloud/src/rie6/client/admin/model/User.java
@@ -18,31 +18,57 @@ public class User {
 		this.status = status;	
 	}
 
+	
 	//*****  GETTEURS ****
 	
 	public String getId() {
 		return id;
 	}
 
+	public void setId(String id) {
+		this.id = id;
+	}
+
 	public String getLogin() {
 		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
 	}
 
 	public String getMdp() {
 		return mdp;
 	}
 
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+
 	public String getPrenom() {
 		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
 	}
 
 	public String getMail() {
 		return mail;
 	}
 
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
 	public String getStatus() {
 		return status;
 	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
 	
 	
 }
--- trunk/ kiscloud/KisCloud/src/rie6/client/model/RPCservice.java
+++ trunk/ kiscloud/KisCloud/src/rie6/client/model/RPCservice.java
@@ -1,5 +1,9 @@
 package rie6.client.model;
 
+import java.util.List;
+
+import rie6.client.admin.model.User;
+
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 /**
@@ -13,5 +17,7 @@ public interface RPCservice extends RemoteService {
 	String[] testThisCookie(String id_cookie);
 
 	String[] checkThisLoginNpwd(String login, String pwd);
+	
+	public List<User> getListUser();
 
 }
