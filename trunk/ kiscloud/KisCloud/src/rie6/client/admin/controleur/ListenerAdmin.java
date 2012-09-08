3dc4d5937a1a415e39b1b29bd6a7a9538f4deaaf
sylvain.pellissier@gmail.com <sylvain.pellissier@gmail.com@4320ce56-c730-6eaf-f33c-faf3ef3ae2b0>
2012-09-08 10:26:36 +0000

    view database OK

--- trunk/ kiscloud/KisCloud/src/rie6/client/admin/controleur/ListenerAdmin.java
+++ trunk/ kiscloud/KisCloud/src/rie6/client/admin/controleur/ListenerAdmin.java
@@ -8,85 +8,89 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-
 public class ListenerAdmin implements ClickHandler {
 
-	private ControllerAdmin controllerAdmin;
-	private PageManageUser pageManageUser;
-	private String id = null;
-	
-	public ListenerAdmin(String id, ControllerAdmin controllerAdmin){
-		
-		this.controllerAdmin =controllerAdmin;
-		this.id=id;
-	}
-	
-	@Override
-	public void onClick(ClickEvent event) {
-		
-		//**** MENU BUTTON ****// 
-		
-		if (id.equals("buttonManageUser")){		
-			pageManageUser = new PageManageUser(controllerAdmin);	
-		}
-		
-		if (id.equals("buttonManageNode")){		
-			
-		}
-		
-		if (id.equals("buttonViewRessources")){		
-			
-		}
-		
-		if (id.equals("buttonListVMUsers")){		
-			
-		}
-		if (id.equals("buttonLogs")){		
-			
-		}
-		
-		//**** PAGE MANAGE USER **** //
-		
-		if (id.equals("buttonAddUser")){		
-			System.out.println("je clique pour ajouter un utilisateur dans la base");
-		}
-		
-		if (id.equals("buttonModifyUser")){	
-			System.out.println("je clique pour modifier un utilisateur dans la base");
-			
-			
-			final DialogBox dialogBox = new DialogBox();
-			dialogBox.setGlassEnabled(true);
-		    dialogBox.setAnimationEnabled(true);
-			
-		    VerticalPanel dialogContents = new VerticalPanel();
-		    dialogContents.setSpacing(4);
-		    dialogBox.setWidget(dialogContents);
-		    
-		    Button closeButton = new Button();
-		    closeButton.addClickHandler(new ClickHandler() {
-		              public void onClick(ClickEvent event) {
-		                dialogBox.hide();
-		              }
-		            });
-		    
-		    dialogContents.add(closeButton);
-		    dialogBox.show();
-		   			
-		}
-		
-		if (id.equals("buttonDeleteUser")){		
-			System.out.println("je clique pour supprimer un utilisateur dans la base");
-		}
+    private final ControllerAdmin controllerAdmin;
+    private PageManageUser        pageManageUser;
+    private String                id = null;
 
-	}
+    public ListenerAdmin(String id, ControllerAdmin controllerAdmin) {
 
-	public ControllerAdmin getControllerAdmin() {
-		return controllerAdmin;
-	}
+        this.controllerAdmin = controllerAdmin;
+        this.id = id;
+    }
 
-	public PageManageUser getPageManageUser() {
-		return pageManageUser;
-	}
-	
+    @Override
+    public void onClick(ClickEvent e) {
+
+        //**** MENU BUTTON ****// 
+
+        if (id.equals("buttonManageUser")) {
+            pageManageUser = new PageManageUser(controllerAdmin);
+        }
+
+        if (id.equals("buttonManageNode")) {
+            System.out.println("je clique pour ManageNode");
+        }
+
+        if (id.equals("buttonViewRessources")) {
+            System.out.println("je clique sur ViewRessources");
+        }
+
+        if (id.equals("buttonListVMUsers")) {
+            System.out.println("je clique sur ListVMUsers");
+
+        }
+        if (id.equals("buttonLogs")) {
+
+        }
+
+        //**** PAGE MANAGE USER **** //
+
+        if (id.equals("buttonAddUser")) {
+            System.out.println("je clique pour ajouter un utilisateur dans la base");
+
+            Button buttonAdd = (Button) e.getSource();
+            PageManageUser pageManageUser = (PageManageUser) buttonAdd.getParent();
+
+        }
+
+        if (id.equals("buttonModifyUser")) {
+            System.out.println("je clique pour modifier un utilisateur dans la base");
+
+            final DialogBox dialogBox = new DialogBox();
+            dialogBox.setGlassEnabled(true);
+            dialogBox.setAnimationEnabled(true);
+
+            VerticalPanel dialogContents = new VerticalPanel();
+            dialogContents.setSpacing(4);
+            dialogBox.setWidget(dialogContents);
+
+            Button closeButton = new Button();
+            closeButton.addClickHandler(new ClickHandler() {
+                @Override
+                public void onClick(ClickEvent event) {
+                    dialogBox.hide();
+                }
+            });
+
+            dialogContents.add(closeButton);
+            dialogBox.show();
+
+        }
+
+        if (id.equals("buttonDeleteUser")) {
+            System.out.println("je clique pour supprimer un utilisateur dans la base");
+        }
+
+    }
+
+    public ControllerAdmin getControllerAdmin() {
+        return controllerAdmin;
+    }
+
+    public PageManageUser getPageManageUser() {
+        return pageManageUser;
+    }
+
 }
