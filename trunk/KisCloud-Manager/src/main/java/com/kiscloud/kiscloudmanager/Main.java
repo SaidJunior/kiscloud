8b3608a4e1a3b01a28272c3b0f7cdf29754c93b0
chabert.clem <chabert.clem@4320ce56-c730-6eaf-f33c-faf3ef3ae2b0>
2012-09-18 08:30:36 +0000

    KISCloud Manager Init

---
+++ trunk/KisCloud-Manager/src/main/java/com/kiscloud/kiscloudmanager/Main.java
@@ -0,0 +1,24 @@
+package com.kiscloud.kiscloudmanager;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+
+    private static final Logger logger = Logger.getLogger(Main.class);
+
+    public Main(){
+        logger.log(Level.INFO, "Main Start");
+    }
+    
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Main main = new Main();
+    }
+}
