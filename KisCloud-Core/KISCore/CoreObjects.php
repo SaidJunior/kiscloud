dea0453788894633110031368a60f48cca391825
chabert.clem <chabert.clem@gmail.com>
2012-09-25 07:07:34 +0000

    First php

---
+++ KisCloud-Core/KISCore/CoreObjects.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of CoreObjects
+ *
+ * @author clement
+ */
+
+include 'CoreObjects/CoreNode.php';
+
+class CoreObjects {
+    //put your code here
+    
+    public function __construct() {
+        
+    }
+}
+
+?>
---
+++ KisCloud-Core/KISCore/CoreObjects/CoreNode.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of CoreNode
+ *
+ * @author clement
+ */
+class CoreNode extends CoreObjects{
+    //put your code here
+    
+    private $vtd_enabled = null;
+    private $vtd_type = null;
+    
+    public function __construct() {
+        parent::__construct();
+        
+    }
+    
+    public function getVtd_enabled() {
+        return $this->vtd_enabled;
+    }
+
+    public function setVtd_enabled($vtd_enabled) {
+        $this->vtd_enabled = $vtd_enabled;
+    }
+
+    public function getVtd_type() {
+        return $this->vtd_type;
+    }
+
+    public function setVtd_type($vtd_type) {
+        $this->vtd_type = $vtd_type;
+    }
+
+}
+
+?>
