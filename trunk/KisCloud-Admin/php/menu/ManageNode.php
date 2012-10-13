be83534a114b2dd0647d1edddc7ac545fdc72fd8
chabert.clem@gmail.com <chabert.clem@gmail.com@4320ce56-c730-6eaf-f33c-faf3ef3ae2b0>
2012-10-13 17:22:24 +0000

    Ajax pannel + Manage Users Modal

--- trunk/KisCloud-Admin/php/menu/ManageNode.php
+++ trunk/KisCloud-Admin/php/menu/ManageNode.php
@@ -1,4 +1,4 @@
 <!-- Partie Manage Node --> 
-    <div id="node" class="tab-pane" >
+    
          <p>Section Manage Node.</p>				         
-    </div>
+
--- trunk/KisCloud-Admin/php/menu/VmUsers.php
+++ trunk/KisCloud-Admin/php/menu/VmUsers.php
@@ -1,4 +1,4 @@
 <!-- Partie Manage VM Users --> 
-    <div id="vm" class="tab-pane" >
+    
          <p>Section VM Users.</p>				         
-    </div>
+
